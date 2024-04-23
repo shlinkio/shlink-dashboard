4df95def0e8fdc06a34b127ef53992687627c4de
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-04-23 09:54:44 +0200

    Pass settings and prefix to ShlinkWebComponent

--- app/routes/server.$serverId.$.tsx
+++ app/routes/server.$serverId.$.tsx
@@ -1,21 +1,38 @@
 import { ServerInfo, ShlinkApiClient } from '@shlinkio/shlink-js-sdk';
 import { FetchHttpClient } from '@shlinkio/shlink-js-sdk/browser';
-import { ReactNode, useEffect, useState } from 'react';
-import { useLoaderData } from '@remix-run/react';
+import { ReactNode, useEffect, useMemo, useState } from 'react';
+import { useLoaderData, useMatches } from '@remix-run/react';
+import type { Settings } from '@shlinkio/shlink-web-component';
 
-export async function loader(): Promise<ServerInfo> {
+export async function loader(): Promise<{ server: ServerInfo; settings: Settings }> {
   return {
-    apiKey: '',
-    baseUrl: '',
+    server: {
+      apiKey: '',
+      baseUrl: '',
+    },
+    settings: {},
   };
 }
 
 export default function ShlinkWebComponentContainer() {
   const [component, setComponent] = useState<ReactNode>(null);
-  const serverInfo = useLoaderData<ReturnType<typeof loader>>();
+  const info = useLoaderData<ReturnType<typeof loader>>();
+  const matches = useMatches();
+  const prefix = useMemo(() => {
+    const currentMatch = matches[matches.length - 1];
+    if (!currentMatch) {
+      return undefined;
+    }
+
+    const { params, pathname } = currentMatch;
+    const rest = params['*'] ?? '';
+    const [pathPrefix] = pathname.split(rest);
+
+    return pathPrefix.endsWith('/') ? pathPrefix.slice(0, -1) : pathPrefix;
+  }, [matches]);
 
   useEffect(() => {
-    const apiClient = new ShlinkApiClient(new FetchHttpClient(), serverInfo);
+    const apiClient = new ShlinkApiClient(new FetchHttpClient(), info.server);
     Promise.all([
       // Just by importing shlink-web-component, some dependencies (like `leaflet`) try to access the window object.
       // That won't work while SSRing, so we need to dynamically import as a side effect, which will happen in the
@@ -24,9 +41,15 @@ export default function ShlinkWebComponentContainer() {
       apiClient.health(),
     ]).then(([{ ShlinkWebComponent }, { version }]) => {
       setComponent(
-        <ShlinkWebComponent serverVersion={version as any} apiClient={apiClient} routesPrefix="/server/123" />
+        <ShlinkWebComponent
+          serverVersion={version as any}
+          apiClient={apiClient}
+          routesPrefix={prefix}
+          settings={info.settings}
+          // tagColorsStorage={} // TODO
+        />
       );
-    })
+    });
   }, []);
 
   return component;
