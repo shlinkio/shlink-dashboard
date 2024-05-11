f96cd0921f8d5e0c802df1e583f3988baf710664
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-05-11 17:48:48 +0200

    Add test for server..tags.colors route

---
+++ app/servers/ServersService.server.ts
@@ -0,0 +1,17 @@
+import type { EntityManager } from 'typeorm';
+import { appDataSource } from '../db/data-source.server';
+import type { Server } from '../entities/Server';
+import { ServerEntity } from '../entities/Server';
+
+export class ServersServiceServer {
+  constructor(private readonly em: EntityManager = appDataSource.manager) {}
+
+  public async getByPublicId(publicId: string): Promise<Server> {
+    const server = await this.em.findOneBy(ServerEntity, { publicId });
+    if (!server) {
+      throw new Error(`Server with public ID ${publicId} not found`);
+    }
+
+    return server;
+  }
+}
---
+++ test/routes/server.$serverId.tags.colors.test.ts
@@ -0,0 +1,24 @@
+import type { ActionFunctionArgs } from '@remix-run/node';
+import { fromPartial } from '@total-typescript/shoehorn';
+import { action } from '../../app/routes/server.$serverId.tags.colors';
+import type { TagsService } from '../../app/tags/TagsService.server';
+
+describe('server.$serverId.tags.colors', () => {
+  const updateTagColors = vi.fn();
+  let tagsService: TagsService;
+
+  beforeEach(() => {
+    tagsService = fromPartial<TagsService>({ updateTagColors });
+    vi.clearAllMocks();
+  });
+
+  it('updates tags in action and returns response', async () => {
+    const colors = { foo: 'red' };
+    const request = fromPartial<ActionFunctionArgs['request']>({ json: vi.fn().mockResolvedValue(colors) });
+
+    const resp = await action(fromPartial({ params: { serverId: '123' }, request }), tagsService);
+
+    expect(updateTagColors).toHaveBeenCalled();
+    expect(resp.status).toEqual(204);
+  });
+});
