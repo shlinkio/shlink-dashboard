48abb7c45338623b2cfb3fb9ec20050d75258c68
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-05-19 19:09:19 +0200

    Update MainHeader test to cover the absence and presence of the session

--- test/common/MainHeader.test.tsx
+++ test/common/MainHeader.test.tsx
@@ -1,14 +1,37 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { fromPartial } from '@total-typescript/shoehorn';
 import { MemoryRouter } from 'react-router';
+import type { SessionData } from '../../app/auth/session-context';
+import { SessionProvider } from '../../app/auth/session-context';
 import { MainHeader } from '../../app/common/MainHeader';
 import { checkAccessibility } from '../__helpers__/accessibility';
 
 describe('<MainHeader />', () => {
-  const setUp = () => render(
-    <MemoryRouter>
-      <MainHeader />
-    </MemoryRouter>,
+  const setUp = (session?: SessionData) => render(
+    <SessionProvider value={session ?? null}>
+      <MemoryRouter>
+        <MainHeader />
+      </MemoryRouter>
+    </SessionProvider>,
   );
 
-  it('passes a11y checks', () => checkAccessibility(setUp()));
+  it.each([
+    [undefined],
+    [fromPartial<SessionData>({})],
+  ])('passes a11y checks', (session) => checkAccessibility(setUp(session)));
+
+  it.each([
+    [undefined],
+    [fromPartial<SessionData>({})],
+  ])('shows logout and menu toggle only if session is set', (session) => {
+    setUp(session);
+
+    if (session) {
+      expect(screen.getByRole('button')).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Logout' })).toBeInTheDocument();
+    } else {
+      expect(screen.queryByRole('button')).not.toBeInTheDocument();
+      expect(screen.queryByRole('link', { name: 'Logout' })).not.toBeInTheDocument();
+    }
+  });
 });
