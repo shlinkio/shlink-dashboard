1841298fa3b1b0add95fa99583acdc3661bdc0da
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-03-07 12:34:28 +0100

    Add eslint-plugin-react-compiler

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import shlink from '@shlinkio/eslint-config-js-coding-standard';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 // eslint-disable-next-line no-restricted-exports
 export default [
   ...shlink,
+  reactCompiler.configs.recommended,
   {
     // Allow config files and route files to have a default export, as that's what the third parties consuming them
     // expect
