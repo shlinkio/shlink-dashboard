ebc3cea739f4a82a6a87169e0abb945585a8443f
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-03-13 20:20:40 +0100

    Add support for solid buttons

--- app/fe-kit/Button.tsx
+++ app/fe-kit/Button.tsx
@@ -10,9 +10,10 @@ type LinkButtonProps = LinkProps;
 export type ButtonProps = PropsWithChildren<{
   disabled?: boolean;
   className?: string;
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary'; /* 'danger' */
   size?: Size;
   inline?: boolean;
+  solid?: boolean;
 } & (RegularButtonProps | LinkButtonProps)>;
 
 export const Button: FC<ButtonProps> = ({
@@ -22,6 +23,7 @@ export const Button: FC<ButtonProps> = ({
   variant = 'primary',
   size = 'md',
   inline = false,
+  solid = false,
   ...rest
 }) => {
   const Tag = 'to' in rest ? Link : 'button';
@@ -41,13 +43,23 @@ export const Button: FC<ButtonProps> = ({
           'tw:px-2 tw:py-1 tw:text-sm': size === 'sm',
           'tw:px-3 tw:py-1.5': size === 'md',
           'tw:px-4 tw:py-2 tw:text-lg': size === 'lg',
+        },
+        {
           'tw:border-shlink-brand tw:text-shlink-brand': variant === 'primary',
-          'tw:border-gray-400 tw:text-gray-400': variant === 'secondary',
+          'tw:border-zinc-400 tw:text-zinc-400': variant === 'secondary',
+        },
+        solid && {
+          'tw:text-white! tw:bg-shlink-brand': variant === 'primary',
+          'tw:hover:bg-shlink-brand-dark tw:hover:border-shlink-brand-dark': variant === 'primary',
+          'tw:focus:bg-shlink-brand-dark tw:focus:border-shlink-brand-dark': variant === 'primary',
+
+          'tw:text-black! tw:bg-zinc-400': variant === 'secondary',
+          'tw:hover:bg-zinc-500 tw:hover:border-zinc-500 tw:focus:bg-zinc-500 tw:focus:border-zinc-500': variant === 'secondary',
         },
         !disabled && {
-          'tw:hover:text-white! tw:focus:text-white!': true,
+          'tw:hover:text-white! tw:focus:text-white!': !solid,
           'tw:hover:bg-shlink-brand tw:focus:bg-shlink-brand': variant === 'primary',
-          'tw:hover:bg-gray-400 tw:focus:bg-gray-400': variant === 'secondary',
+          'tw:hover:bg-zinc-400 tw:focus:bg-zinc-400': variant === 'secondary',
         },
         {
           'tw:pointer-events-none tw:opacity-65': disabled,
