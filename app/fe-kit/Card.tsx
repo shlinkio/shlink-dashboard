e6d49b31ce1784a8d73d4d56160bb9a87fe7ca49
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-03-20 09:44:18 +0100

    Fix borders in Card component

--- app/fe-kit/Card.tsx
+++ app/fe-kit/Card.tsx
@@ -6,7 +6,7 @@ export type CardProps = HTMLProps<HTMLDivElement>;
 const Header: FC<CardProps> = ({ className, ...rest }) => (
   <div
     className={clsx(
-      'tw:px-4 tw:py-3 tw:bg-(--primary-color) tw:border tw:border-(--border-color) tw:rounded-t-md',
+      'tw:px-4 tw:py-3 tw:bg-(--primary-color) tw:border-b tw:border-(--border-color) tw:rounded-t-md',
       className,
     )}
     {...rest}
@@ -17,7 +17,6 @@ const Body: FC<CardProps> = ({ className, ...rest }) => (
   <div
     className={clsx(
       'tw:p-4 tw:bg-(--primary-color) tw:first:rounded-t-md',
-      'tw:border-x tw:first:border-t tw:last:border-b tw:border-(--border-color)',
       'tw:first:rounded-t-md tw:last:rounded-b-md',
       className,
     )}
@@ -28,7 +27,7 @@ const Body: FC<CardProps> = ({ className, ...rest }) => (
 const Footer: FC<CardProps> = ({ className, ...rest }) => (
   <div
     className={clsx(
-      'tw:px-4 tw:py-3 tw:bg-(--primary-color) tw:border tw:border-(--border-color) tw:rounded-b-md',
+      'tw:px-4 tw:py-3 tw:bg-(--primary-color) tw:border-t tw:border-(--border-color) tw:rounded-b-md',
       className,
     )}
     {...rest}
@@ -36,7 +35,12 @@ const Footer: FC<CardProps> = ({ className, ...rest }) => (
 );
 
 const BaseCard: FC<CardProps> = ({ className, ...props }) => (
-  <div className={clsx('tw:group/card tw:shadow-md', className)} {...props} />
+  <div
+    className={clsx(
+      'tw:group/card tw:rounded-md tw:shadow-md tw:border tw:border-(--border-color) tw:bg-(--primary-color)',
+      className)}
+    {...props}
+  />
 );
 
 export const Card = Object.assign(BaseCard, { Body, Header, Footer });
