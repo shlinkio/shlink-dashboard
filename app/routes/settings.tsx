b63873b10afadf9ba8e5d881cf023655c7fed8eb
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-03-29 13:26:19 +0100

    Update to latest coding standard

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -2,8 +2,7 @@ import type { Settings as AppSettings } from '@shlinkio/shlink-web-component/set
 import { ShlinkWebSettings } from '@shlinkio/shlink-web-component/settings';
 import { useCallback } from 'react';
 import type { ActionFunctionArgs, LoaderFunctionArgs } from 'react-router';
-import { Route, Routes } from 'react-router';
-import { useFetcher, useLoaderData } from 'react-router';
+import { Route, Routes , useFetcher, useLoaderData } from 'react-router';
 import { AuthHelper } from '../auth/auth-helper.server';
 import { Layout } from '../common/Layout';
 import { serverContainer } from '../container/container.server';
