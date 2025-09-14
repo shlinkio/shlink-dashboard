3880722b8849c4d8757c45297b86afa52acced50
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-09-14 10:43:15 +0200

    Update types for now stable react-router middleware

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,3 @@
 import type { Config } from '@react-router/dev/config';
 
-export default {
-  future: {
-    unstable_middleware: true,
-  },
-} satisfies Config;
+export default {} satisfies Config;
